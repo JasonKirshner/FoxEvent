ccba1815fa2cfb635ad2fa18b82675afaadb35f9
Jason Kirshner <jasonkirshner13@gmail.com>
2019-09-23 10:54:48 -0400

    added jquery and css animations
    Signed-off-by: Jason Kirshner <jasonkirshner13@gmail.com>

--- js/main.js
+++ js/main.js
@@ -2,30 +2,51 @@ history.scrollRestoration = "manual"
 
 $(document).ready(() => {
 	var flag = true
-	var desc = $('.top').position().top - 200
+	var desc = $('.desc').position().top - 300
 	var highlights = $('.highlights').position().top - 300
 	var map = $('.map').position().top - 300
 
-	console.log(highlights)
+	$('.hero img').addClass('pop-up')
+
+	$(this).delay(2000).queue(() => {
+		$('.hero img').css('top', '1rem').removeClass('pop-up')
+	})
 
 	$(window).scroll(() => {
 		var pos = $(window).scrollTop()
+
+		// Displays sections
 		switch (flag) {
 			case pos > desc && pos < highlights:
-				$('.top .container').animate({
+				$('.desc .container').animate({
 					'opacity': 1
-				}, 400)
+				}, 500)
 				break
 			case pos > highlights && pos < map:
+				$('.highlights h2').animate({
+					'opacity': 1
+				})
 				$('.highlights .container').animate({
 					'opacity': 1
-				}, 400)
+				}, 500)
 				break
 			case pos > map:
+				$('.map h2').animate({
+					'opacity': 1
+				})
 				$('.map .container').animate({
 					'opacity': 1
-				}, 400)
+				}, 500)
 				flag = false
 		}
+
+		if (pos > desc)
+			$('.navbar').css({
+				backgroundColor: 'rgba(0,0,0,.5)'
+			})
+		else
+			$('.navbar').css({
+				background: 'none'
+			})
 	})
 })
